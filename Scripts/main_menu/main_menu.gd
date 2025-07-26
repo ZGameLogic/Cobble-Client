a52ad8df9b6ac3110e4da8a9e486599ad3c96112
Ben Shabowski <benshabowski@gmail.com>
2025-07-26 09:08:23 -0500

    Bep

--- Scripts/main_menu/main_menu.gd
+++ Scripts/main_menu/main_menu.gd
@@ -3,7 +3,11 @@ extends Node
 var username_label: RichTextLabel
 
 func login():
-	print("pushed")
+	WebSocketManager.open_login_browser()
+	print("loging in")
+	
+func continue_start_game():
+	print("game")
 
 func _ready():
 	username_label = $TopRight/VBoxContainer/UserNameLabel
